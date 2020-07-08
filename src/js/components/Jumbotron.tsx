c61fc863e5c500b4992fb9889b6328c7f07def33
Erick Christian <erickchristian48@gmail.com>
2020-07-08 15:50:57 +0700

    Changed Jumbotron bg color

--- src/js/components/Jumbotron.tsx
+++ src/js/components/Jumbotron.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 const Jumbotron: React.FC = () => {
   return (
-    <div className='jumbotron container bg-transparent'>
+    <div className='jumbotron container'>
       <h1 className='display-4'>Doggo Getter</h1>
       <p>Get a doggo pic and name it.</p>
     </div>

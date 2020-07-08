a12afe6c31b1fa24cee31bd9300d6afc8252e1c0
Erick Christian <erickchristian48@gmail.com>
2020-07-08 21:44:08 +0700

    Done mockup saveddogs

--- src/js/pages/SavedDogs.tsx
+++ src/js/pages/SavedDogs.tsx
@@ -10,9 +10,11 @@ class SavedDogs extends React.Component {
               <img
                 src='https://upload.wikimedia.org/wikipedia/commons/0/03/Vulpes_vulpes_laying_in_snow.jpg'
                 alt='Fox'
+                className='card-img-top'
               />
               <div className='card-body'>
-                <h5>Fox go Floof</h5>
+                <h5 className='card-title'>Fox go Floof</h5>
+                <button className='btn btn-danger'>Delete</button>
               </div>
             </div>
           </div>
